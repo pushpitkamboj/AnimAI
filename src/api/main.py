2ae4d1ee54a16fc2a559782cbffcf91f6f917873
pushpit kamboj <pushpitkamboj@gmail.com>
2025-10-23 18:18:20 +0530

    added cors middleware

--- src/api/main.py
+++ src/api/main.py
@@ -5,6 +5,16 @@ from pydantic import BaseModel
 from agent.graph import workflow_app
 from langgraph.errors import GraphRecursionError
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specific frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class InstructionInput(BaseModel):
