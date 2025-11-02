fda9382efe11c5df3d7e7d0ea925c7ff5c98a390
Ansh Bansal <ansh.interschools@gmail.com>
2025-11-02 21:23:53 +0530

    gpt5

--- src/agent/regenerate_code.py
+++ src/agent/regenerate_code.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from langgraph.graph import END
 from langchain.chat_models import init_chat_model
 
-llm = init_chat_model("openai:gpt-4.1")
+llm = init_chat_model("openai:gpt-5")
 
 
 class output_code(BaseModel): 
